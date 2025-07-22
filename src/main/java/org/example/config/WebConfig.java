e11b26db9e29c3ef263417637b12e8e8e3594578
Anton Volkov <66030600+AHTOHFANTOM@users.noreply.github.com>
2025-07-22 11:21:26 +0300

    изменил порт для dev версии и внес правки в редактор кода

--- src/main/java/org/example/config/WebConfig.java
+++ src/main/java/org/example/config/WebConfig.java
@@ -14,10 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**") // или "/**" чтобы разрешить на весь API
-                        .allowedOrigins("http://172.19.0.1:3000", "http://95.213.143.142") // разрешённый фронтенд
+                        .allowedOrigins("http://172.19.0.1:3000", "http://95.213.143.142:4000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true); // если используете куки или авторизацию
+                        .allowCredentials(true);
             }
         };
     }

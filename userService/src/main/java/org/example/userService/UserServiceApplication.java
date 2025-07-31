a896c7b76b76022f5e21777a1872fb76c526041e
Антон Волков <aenegdot.kristalika.06@gmail.com>
2025-07-31 18:38:09 +0300

    Внес небольшие правки

--- userService/src/main/java/org/example/userService/UserServiceApplication.java
+++ userService/src/main/java/org/example/userService/UserServiceApplication.java
@@ -4,9 +4,11 @@ import org.example.common.utils.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableConfigurationProperties(CorsProperties.class)
+@EnableJpaRepositories(basePackages = "org.example.common.repository")
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);

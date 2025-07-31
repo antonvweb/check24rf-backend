477a795327dd3e4178e686be5f70a8853d2efd63
Антон Волков <aenegdot.kristalika.06@gmail.com>
2025-07-31 18:41:59 +0300

    Внес небольшие правки

--- userService/src/main/java/org/example/userService/UserServiceApplication.java
+++ userService/src/main/java/org/example/userService/UserServiceApplication.java
@@ -3,12 +3,23 @@ package org.example.userService;
 import org.example.common.utils.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
 @EnableConfigurationProperties(CorsProperties.class)
-@EnableJpaRepositories(basePackages = "org.example.common.repository")
+@SpringBootApplication(scanBasePackages = {
+        "org.example.userService",
+        "org.example.common"  // Сканируем пакеты из common модуля
+})
+@EntityScan(basePackages = {
+        "org.example.userService.entity",  // Локальные entity
+        "org.example.common.entity"        // Entity из common модуля
+})
+@EnableJpaRepositories(basePackages = {
+        "org.example.userService.repository", // Локальные repository
+        "org.example.common.repository"       // Repository из common модуля
+})
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);

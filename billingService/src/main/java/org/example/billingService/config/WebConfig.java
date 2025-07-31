09a30966fe283e42f5f83b4c97647dea9ad43517
Антон Волков <aenegdot.kristalika.06@gmail.com>
2025-07-31 17:11:37 +0300

    Внес небольшие правки

--- billingService/src/main/java/org/example/billingService/config/WebConfig.java
+++ billingService/src/main/java/org/example/billingService/config/WebConfig.java
@@ -1,6 +1,5 @@
 package org.example.billingService.config;
 
-
 import org.example.userService.utils.CorsProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;

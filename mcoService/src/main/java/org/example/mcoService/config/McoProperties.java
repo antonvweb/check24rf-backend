7883d6c92801f69adf18da92de92b5e05d3aa850
Антон Волков <aenegdot.kristalika.06@gmail.com>
2025-10-13 22:03:30 +0300

    ТЕСТ

--- mcoService/src/main/java/org/example/mcoService/config/McoProperties.java
+++ mcoService/src/main/java/org/example/mcoService/config/McoProperties.java
@@ -5,7 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Data
-@Configuration
 @ConfigurationProperties(prefix = "mco")
 public class McoProperties {
 

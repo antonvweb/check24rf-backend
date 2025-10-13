a0959e9926b0941e244c44c0a6d1026f0cf7dcb7
Антон Волков <aenegdot.kristalika.06@gmail.com>
2025-10-13 21:56:29 +0300

    ТЕСТ

--- mcoService/src/main/java/org/example/mcoService/McoIntegrationApplication.java
+++ mcoService/src/main/java/org/example/mcoService/McoIntegrationApplication.java
@@ -1,11 +1,12 @@
 package org.example.mcoService;
 
+import org.example.mcoService.config.McoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties
+@EnableConfigurationProperties(McoProperties.class)
 public class McoIntegrationApplication {
 
     public static void main(String[] args) {

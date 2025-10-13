0438e6e3bf26e4662744e5111d85a10b2f111a33
Антон Волков <aenegdot.kristalika.06@gmail.com>
2025-10-13 21:45:33 +0300

    ТЕСТ

--- mcoService/src/main/java/org/example/mcoService/client/McoSoapClient.java
+++ mcoService/src/main/java/org/example/mcoService/client/McoSoapClient.java
@@ -26,46 +26,7 @@ public class McoSoapClient {
         try {
             log.debug("Отправка SOAP запроса: {}", request.getClass().getSimpleName());
 
-            // Interceptor для добавления токена в заголовок
-            ClientInterceptor tokenInterceptor = new ClientInterceptor() {
-                @Override
-                public boolean handleRequest(MessageContext messageContext) {
-                    if (messageContext.getRequest() instanceof SoapMessage soapMessage) {
-                        try {
-                            soapMessage.getSoapHeader()
-                                    .addHeaderElement(new QName(
-                                            "urn://x-artefacts-gnivc-ru/ais3/kkt/DrPartnersIntegrationService/v0.1",
-                                            "FNS-OpenApi-Token"
-                                    ))
-                                    .setText(mcoProperties.getApi().getToken());
-                        } catch (Exception e) {
-                            log.error("Ошибка добавления токена в SOAP-заголовок", e);
-                        }
-                    }
-                    return true;
-                }
-
-                @Override
-                public boolean handleResponse(MessageContext messageContext) {
-                    return true;
-                }
-
-                @Override
-                public boolean handleFault(MessageContext messageContext) {
-                    return true;
-                }
-
-                @Override
-                public void afterCompletion(MessageContext messageContext, Exception ex) {
-                    // Здесь можно очистить ресурсы или логировать окончание запроса
-                    if (ex != null) {
-                        log.error("Ошибка в afterCompletion SOAP запроса", ex);
-                    }
-                }
-            };
-
-            webServiceTemplate.setInterceptors(new ClientInterceptor[]{tokenInterceptor});
-
+            // Просто используем уже настроенный бин
             Object response = webServiceTemplate.marshalSendAndReceive(
                     mcoProperties.getApi().getBaseUrl(),
                     request

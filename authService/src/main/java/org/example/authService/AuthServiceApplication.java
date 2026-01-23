d55e077a70ea9fee5dc415ead240c32ea1dbab63
Антон Волков <aenegdot.kristalika.06@gmail.com>
2026-01-23 20:47:46 +0300

    mco изменения

--- authService/src/main/java/org/example/authService/AuthServiceApplication.java
+++ authService/src/main/java/org/example/authService/AuthServiceApplication.java
@@ -2,9 +2,13 @@ package org.example.authService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
         "org.example.common"  // Сканируем пакеты из common модуля
+})
+@EnableJpaRepositories(basePackages = {
+        "org.example.common.repository"       // Repository из common модуля
 })
 public class AuthServiceApplication {
     public static void main(String[] args) {
--- mcoService/src/main/java/org/example/mcoService/McoIntegrationApplication.java
+++ mcoService/src/main/java/org/example/mcoService/McoIntegrationApplication.java
@@ -2,11 +2,13 @@ package org.example.mcoService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = {
         "org.example.common"
 })
+@EnableJpaRepositories(basePackages = "org.example.common.repository")  // ← это важно!
 @EnableAsync
 public class McoIntegrationApplication {
 

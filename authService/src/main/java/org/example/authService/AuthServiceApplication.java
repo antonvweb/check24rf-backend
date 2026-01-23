abc4b44065a6b75e9e67d9de6b85b660d2e240e2
Антон Волков <aenegdot.kristalika.06@gmail.com>
2026-01-23 20:42:32 +0300

    mco изменения

--- authService/src/main/java/org/example/authService/AuthServiceApplication.java
+++ authService/src/main/java/org/example/authService/AuthServiceApplication.java
@@ -3,7 +3,9 @@ package org.example.authService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "org.example.common"  // Сканируем пакеты из common модуля
+})
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
--- mcoService/src/main/java/org/example/mcoService/McoIntegrationApplication.java
+++ mcoService/src/main/java/org/example/mcoService/McoIntegrationApplication.java
@@ -1,14 +1,13 @@
 package org.example.mcoService;
 
-import org.example.mcoService.config.McoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "org.example.common"
+})
 @EnableAsync
-@EnableConfigurationProperties(McoProperties.class)
 public class McoIntegrationApplication {
 
     public static void main(String[] args) {

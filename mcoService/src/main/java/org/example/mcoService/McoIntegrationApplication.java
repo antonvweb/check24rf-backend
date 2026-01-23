c4b331dcd075ec41f956c56290fe83faa09e5d6f
Антон Волков <aenegdot.kristalika.06@gmail.com>
2026-01-23 21:38:04 +0300

    mco изменения

--- mcoService/src/main/java/org/example/mcoService/McoIntegrationApplication.java
+++ mcoService/src/main/java/org/example/mcoService/McoIntegrationApplication.java
@@ -1,8 +1,10 @@
 package org.example.mcoService;
 
+import org.example.mcoService.config.McoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -15,6 +17,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 })
 @EnableJpaRepositories(basePackages = "org.example.common.repository")  // ← это важно!
 @EnableAsync
+@EnableConfigurationProperties(McoProperties.class)
 public class McoIntegrationApplication {
 
     public static void main(String[] args) {

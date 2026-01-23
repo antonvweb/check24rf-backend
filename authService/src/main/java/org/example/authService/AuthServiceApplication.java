6bc4fae2413c37e13ca5f23bc5225be0a5f0bd7a
Антон Волков <aenegdot.kristalika.06@gmail.com>
2026-01-23 20:58:50 +0300

    mco изменения

--- authService/src/main/java/org/example/authService/AuthServiceApplication.java
+++ authService/src/main/java/org/example/authService/AuthServiceApplication.java
@@ -2,10 +2,14 @@ package org.example.authService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
         "org.example.common"  // Сканируем пакеты из common модуля
+})
+@EntityScan(basePackages = {
+        "org.example.common.entity"        // Entity из common модуля
 })
 @EnableJpaRepositories(basePackages = {
         "org.example.common.repository"       // Repository из common модуля

e58406c2475ef8171288b17df21e6358136ecfa1
Антон Волков <aenegdot.kristalika.06@gmail.com>
2026-02-12 17:41:46 +0300

    mco изменения

--- authService/src/main/java/org/example/authService/service/EmailService.java
+++ authService/src/main/java/org/example/authService/service/EmailService.java
@@ -5,17 +5,12 @@ import org.springframework.stereotype.Service;
 
 /**
  * Сервис для отправки email
- * TODO: Реализовать интеграцию с email провайдером
  */
 @Slf4j
 @Service
 public class EmailService {
     
     public void sendVerificationCode(String email, String code) {
-        // TODO: Интеграция с email провайдером (SendGrid, AWS SES, и т.д.)
         log.info("📧 [MOCK] Отправка кода на email: {}, код: {}", email, code);
-        
-        // Временная заглушка - код просто логируется
-        // В продакшене здесь должна быть реальная отправка email
     }
 }
